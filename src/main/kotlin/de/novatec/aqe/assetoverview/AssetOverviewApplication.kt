9697dc40b498ad967f6d5ed9b5ca071222eeede4
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-05-26 11:53:12 +0200

    added automatic refresh of once per hour

--- src/main/kotlin/de/novatec/aqe/assetoverview/AssetOverviewApplication.kt
+++ src/main/kotlin/de/novatec/aqe/assetoverview/AssetOverviewApplication.kt
@@ -1,10 +1,28 @@
 package de.novatec.aqe.assetoverview
 
+import de.novatec.aqe.assetoverview.business.ProjectRepository
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.SchedulingConfigurer
+import org.springframework.scheduling.config.ScheduledTaskRegistrar
+import java.util.concurrent.TimeUnit
 
 @SpringBootApplication
-class AssetOverviewApplication
+class AssetOverviewApplication {
+
+    @Configuration
+    @EnableScheduling
+    class SchedulingConfiguration(private val repository: ProjectRepository) : SchedulingConfigurer {
+
+        override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
+            taskRegistrar.addFixedDelayTask(repository::refresh, TimeUnit.HOURS.toMillis(1))
+        }
+
+    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(AssetOverviewApplication::class.java, *args)
